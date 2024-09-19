b8903d5d077e42f07404089ff987efa557504d03
txj <21307110035@m.fudan.edu.cn>
2024-09-19 21:42:25 +0800

    fix: nav no icon

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,19 +2,6 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   {
-    text: 'Home',
-    icon: 'iconfont icon-home',
-    link: '/',
-  },
-  { text: 'Team', children:[
-      { text: 'Attributions', link: '/attributions' },
-      { text: 'Collaborations', link: '/collaborations' },
-      { text: 'Heritage', link: '/heritage' },
-      { text: 'Members', link: '/team' },
-    ],
-    icon: 'iconfont icon-team',
-  },
-  {
     text: 'Project',children:[
       { text: 'Contribution', link: '/contribution' },
       { text: 'Description', link: '/description' },
@@ -28,35 +15,34 @@ export default navbar([
       { text: 'Notebook', link: '/notebook' },
       { text: 'Software', link: '/software' },
       { text: 'Results', link: '/results' },
-    ],
-    icon: 'iconfont icon-project',
+    ]
   },
   {
     text: 'Parts',children:[
       { text: 'Improved Parts', link: '/improve' },
       { text: 'Part Collection', link: '/part-collection' },
       { text: 'Registry', link: 'https://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2024&group=Fudan' },
-    ],
-    icon: 'iconfont icon-experiment',
-  },
-  {
-    text: 'Safety',
-    icon: 'iconfont icon-safetycertificate',
-    link: '/safety',
+    ]
   },
   {
     text: 'Human Practices',children:[
       { text: 'Education', link: '/education' },
+      { text: 'Entrepreneurship', link: '/entrepreneurship' },
       { text: 'Inclusivity', link: '/inclusivity' },
       { text: 'Integrated HP', link: '/human-practices' },
       { text: 'Promotion Video', link: '/promotion-video' },
       { text: 'Sustainable', link: '/sustainable' },
-    ],
-    icon: 'iconfont icon-smile',
+    ]
   },
   {
-    text: 'Entrepreneurship',
-    icon: 'iconfont icon-gold',
-    link: '/entrepreneurship',
+    text: 'Safety',
+    link: '/safety',
   },
+  { text: 'Team', children:[
+      { text: 'Attributions', link: '/attributions' },
+      { text: 'Collaborations', link: '/collaborations' },
+      { text: 'Heritage', link: '/heritage' },
+      { text: 'Members', link: '/team' },
+    ]
+  }
 ],);
