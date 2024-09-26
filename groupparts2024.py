1057a94a459206506f1dd81028af34a29466ab0f
txj <21307110035@m.fudan.edu.cn>
2024-09-26 20:03:13 +0800

    fix

--- groupparts2024.py
+++ groupparts2024.py
@@ -17,7 +17,6 @@ import lxml # 4.9.4-cp312-universal2
 from bs4 import BeautifulSoup # 4.12.3
 from time import sleep
 
-
 z = ['BBa_J18920', 'BBa_K1151001', 'BBa_K4162006', 'BBa_K4765020', 'BBa_K4765021']
 z += ['BBa_K4162001', 'BBa_K4162009', 'BBa_K4162010', 'BBa_K4162011', 'BBa_K4162012',
  'BBa_K4162013', 'BBa_K4162014', 'BBa_K4162016', 'BBa_K4162019', 'BBa_K4162021',
@@ -26,18 +25,16 @@ z += ['BBa_K4162001', 'BBa_K4162009', 'BBa_K4162010', 'BBa_K4162011', 'BBa_K4162
  'BBa_K4765111', 'BBa_K4765112', 'BBa_K4765113', 'BBa_K4765117', 'BBa_K4765126' ] # testing software tool
 z += range(0, 91)
 #z += range(101, 141)
-table_th = ('Part Name', 'Short Description', 'Part Type', 'Designer(s)')
-fff = open('groupparts.md', 'w')
-fff.write('| | | Part Name | Description | Part Type | Designer(s) | Length | Compatible | |\n')
-fff.write('|----|----|----|----|----|----|----|----|----|\n')
 subparts = []
 sub_is_NOT_basic = []
 basic_parts = []
 known_basic_parts = ['BBa_J18920',
 'BBa_K1151001',
+'BBa_K4162001',
 'BBa_K4162006',
 'BBa_K4765020',
 'BBa_K4765021',
+'BBa_K4765022',
 'BBa_K5115000',
 'BBa_K5115001',
 'BBa_K5115002',
@@ -83,6 +80,36 @@ known_basic_parts = ['BBa_J18920',
 'BBa_K5115085',
 'BBa_K5115088',
 'BBa_K5115089' ]
+white_listed = ['BBa_B0030',
+'BBa_I742151',
+'BBa_J61100',
+'BBa_K118002',
+'BBa_K118003',
+'BBa_K118008',
+'BBa_K2150031',
+'BBa_K2306003',
+'BBa_K3457006',
+'BBa_K3648010',
+'BBa_K4162003',
+'BBa_K4162004',
+'BBa_K4162005',
+'BBa_K4162010',
+'BBa_K4162013',
+'BBa_K4162016',
+'BBa_K4162019',
+'BBa_K4765015',
+'BBa_K4765016',
+'BBa_K4765111',
+'BBa_K4765112',
+'BBa_K4765113',
+'BBa_K5115058',
+'BBa_K5115059',
+ ] # not this year, or a fusion protein
+
+table_th = ('Part Name', 'Short Description', 'Part Type', 'Designer(s)')
+fff = open('groupparts.md', 'w')
+fff.write('| | | Part Name | Description | Part Type | Designer(s) | Length | Compatible | |\n')
+fff.write('|----|----|----|----|----|----|----|----|----|\n')
 
 for zz in z:
     if str(zz).startswith('BBa_'):
@@ -103,7 +130,8 @@ for zz in z:
         sleep(10)
         p1 = BeautifulSoup(driver.page_source, features="lxml")
         waiting = 30
-        while not p1.find('span', {'class': 'SnF_partSeqLength legend'}) and waiting > 0:
+        while (not p1.find('span', {'class': 'SnF_partSeqLength legend'}) or
+               not p1.find('table', {'id' : 'subpart_table'}) ) and waiting > 0:
             sleep(2)
             p1 = BeautifulSoup(driver.page_source, features="lxml")
             waiting -= 1
@@ -154,7 +182,7 @@ for zz in z:
                     fff.write('%s ' % inp['value'] )
                     if inp['value'] not in subparts:
                         subparts.append( inp['value'] )
-                        if inp['value'] not in known_basic_parts: # white_list_some
+                        if inp['value'] not in known_basic_parts and inp['value'] not in white_listed:
                             sub_is_NOT_basic.append( inp['value'] )
             print('__ subpart_table count %d BBa_' % subpartss)
             fff.write('|\n')
@@ -174,12 +202,43 @@ for zz in z:
 fff.write('\n\n| | | Old Part | Description | Type | Not 2024 | Length | Compatible | |\n')
 fff.write('|----|----|----|----|----|----|----|----|----|\n')
 fff.close()
+
 print('\n\n====\nBelow are subparts in composite parts:\n')
 print('\n'.join(["'%s'," % x for x in sorted(subparts) ]))
+
 print('\n====\nBelow are basic parts:\n')
 print('\n'.join(["'%s'," % x for x in sorted(basic_parts) ]))
+
 print('\n====\nSubparts are Not basic, and not white listed:\n')
 print('\n'.join(["'%s'," % x for x in sorted(sub_is_NOT_basic) ]))
+# 'BBa_K5115011',
+# 'BBa_K5115012',
+# 'BBa_K5115013',
+# 'BBa_K5115014',
+# 'BBa_K5115015',
+# 'BBa_K5115016',
+# 'BBa_K5115017',
+# 'BBa_K5115018',
+# 'BBa_K5115019',
+# 'BBa_K5115033',
+# 'BBa_K5115035',
+# 'BBa_K5115036',
+# 'BBa_K5115039',
+# 'BBa_K5115052',
+# 'BBa_K5115053',
+# 'BBa_K5115060',
+# 'BBa_K5115061',
+# 'BBa_K5115062',
+# 'BBa_K5115063',
+# 'BBa_K5115064',
+# 'BBa_K5115065',
+# 'BBa_K5115077',
+# 'BBa_K5115078',
+# 'BBa_K5115079',
+# 'BBa_K5115080',
+# 'BBa_K5115081',
+# 'BBa_K5115082',
+
 print('\n\nCAUTION: remove files in parts-html for update\n')
 #print('Validate with https://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2024&group=Fudan\n\n\n\n')
 driver.quit()
